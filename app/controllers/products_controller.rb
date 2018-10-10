57e12de7a4d68b303ec84552305bf0382c4a9c3b
ishwaryaa-balaji <ishwaryaa.balaji@freshworks.com>
2018-10-10 18:40:39 +0530

    Changes on pricing action & success ribbon
    Signed-off-by: ishwaryaa-balaji <ishwaryaa.balaji@freshworks.com>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,20 +12,21 @@ class ProductsController < ApplicationController
    'Deal value' => Product.find_by_id(params[:id]).product_rate
   }
 }
-  FreshsalesAnalytics::identify(cookies[:user_email], sample_properties)
-  	 redirect_to products_path(alert: true)
+
+  FreshsalesAnalytics::identify(cookies[:user_email], sample_properties) 	 
  	flash[:alert] = "Product added to cart successfully"
+  redirect_to products_path(alert: true)
   	FreshsalesAnalytics::trackEvent(cookies[:user_email], 'Product added to cart successfully')
   end
 
    def add_wish
-  	 redirect_to products_path(alert: true)
+  redirect_to products_path(alert: true)
  	flash[:alert] = "Product added to wish list successfully"
   	FreshsalesAnalytics::trackEvent(cookies[:user_email], 'Product added to wish list successfully')
   end
 
   def checkout
-  	 redirect_to products_path(alert: true)
+  redirect_to products_path(alert: true)
  	flash[:alert] = "Thanks for buying"
   	FreshsalesAnalytics::trackEvent(cookies[:user_email], 'Checkout')
   end
